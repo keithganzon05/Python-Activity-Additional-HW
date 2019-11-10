416c239b834a8b29c813a892720d1a4ebac7a3dc
keithganzon05 <57346963+keithganzon05@users.noreply.github.com>
2019-11-10 23:55:25 +0800

    Add files via upload

---
+++ Leap-Year-HW.py
@@ -0,0 +1,8 @@
+Year = int(input("Enter a year: "))
+if(Year%4 == 0):
+    if(Year%100 == 0):
+        if(Year%400 == 0):
+            print(Year, "is a leap year")
+        else: print(Year, "is not a leap year")
+    else: print(Year, "is a leap year")
+else: print(Year, "is not a leap year")
---
+++ Sum-of-Squares-HW.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the number of natural numbers: "))
+s = 0
+x= 0
+y = 1
+while y <= n:
+    y+=1 
+    s+=1 #s = 1,2
+    m = s*s #m = 1,4
+    x+=m #x = 1,5
+else:
+    print(x, "is the sum of the squares")
